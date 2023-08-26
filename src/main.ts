d4fb69f57294330a8b0fc13dba7c7009fac53ae3
nitesh-dev <niteshdev547@gmail.com>
2023-08-26 09:18:42 +0530

    Initial Commit

---
+++ src/main.ts
@@ -0,0 +1,17 @@
+import express from "express";
+import env from 'dotenv'
+env.config()
+
+console.log(process.env.TEST)
+
+const app = express();
+const port = 3000;
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
+
